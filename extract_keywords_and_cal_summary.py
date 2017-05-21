12391b21516f682ddbbdb65c608efafbc3610256
Zhiye Yang <445922703@qq.com>
2017-05-21 20:29:41 +0800

    tfidf cal summary

--- extract_keywords_and_cal_summary.py
+++ extract_keywords_and_cal_summary.py
@@ -1,8 +1,10 @@
 #-*- coding:utf-8 -*-
 from gensim import models,corpora
+from textrank4zh import TextRank4Keyword
 import get_data
 import re
 import heapq
+
 
 def get_max_k_tuple_list (tuple_list , k):
 	
